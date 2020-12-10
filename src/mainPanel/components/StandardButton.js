b5c1de347a1afe63eebc0f0268f2c70068e8a401
DeltaFoxtrot01 <david.dmr2008@gmail.com>
2020-12-10 19:38:43 +0000

    corrected logout bug

--- src/mainPanel/components/StandardButton.js
+++ src/mainPanel/components/StandardButton.js
@@ -22,7 +22,7 @@ export default function StandardButton(props) {
 
   return (
     <ThemeProvider theme={theme}>
-      <Button style={{maxHeight: "80px"}} color={props.color} variant="contained" {...props}>
+      <Button style={{maxHeight: "80px"}} onClick={props.onClick} color={props.color} variant="contained" {...props}>
         {props.children}
       </Button>
     </ThemeProvider>
